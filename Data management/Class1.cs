c29d816f267dabfd485d61990be5adc8b499441f
DanyloKam@gmail.com <DanyloKam@gmail.com>
2020-03-01 14:37:46 +0200

    розбив на папочки

---
+++ Data management/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Data_management
+{
+    public class Class1
+    {
+    }
+
+    public class LoadData
+    {
+
+    }
+}
---
+++ Data management/Serializator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace Data_management
+{
+    interface ISerialization
+    {
+        void Serialization(Object obj, string path);
+        Object Deserialization(string path);
+    }
+
+    class XMLSerializator : ISerialization
+    {
+        DataContractSerializer xmlSerializer;
+        public XMLSerializator(Type type)
+        {
+            xmlSerializer = new DataContractSerializer(type);
+        }
+        public void Serialization(Object obj, string path)
+        {
+            using (FileStream stream = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                xmlSerializer.WriteObject(stream, obj);
+            }
+        }
+
+        public Object Deserialization(string path)
+        {
+            using (FileStream stream = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                return xmlSerializer.ReadObject(stream);
+            }
+        }
+    }
+
+    class JsonSerializator : ISerialization
+    {
+        DataContractJsonSerializer jsonSerializer;
+        public JsonSerializator(Type type)
+        {
+            jsonSerializer = new DataContractJsonSerializer(type);
+        }
+        public void Serialization(Object obj, string path)
+        {
+            using (FileStream stream = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                jsonSerializer.WriteObject(stream, obj);
+            }
+        }
+        public Object Deserialization(string path)
+        {
+            FileStream stream = new FileStream(path, FileMode.OpenOrCreate);
+            {
+                return jsonSerializer.ReadObject(stream);
+            }
+        }
+
+    }
+
+    class Serializator
+    {
+        private ISerialization ser;
+        public Serializator(ISerialization serial)
+        {
+            ser = serial;
+        }
+        public void Serialize(Object obj, string path)
+        {
+            ser.Serialization(obj, path);
+        }
+
+        public Object Deserialize(string path)
+        {
+            return ser.Deserialization(path);
+        }
+    }
+}
